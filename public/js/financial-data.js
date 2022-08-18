ce6fa35608f4bda36e0aaf7720df068ef6e35fdc
antonio-datahack <antonio.datahack@gmail.com>
2022-08-18 15:54:09 +0200

    almost done

--- public/js/financial-data.js
+++ public/js/financial-data.js
@@ -1,58 +1,41 @@
-//const apiUrl = "http://api.coindesk.com/v1/bpi/historical/close.json?start=${fromDate}&end=${toDate}&currency=${currency}";
+let fromDate = document.getElementById("from").valueAsDate;
+let toDate = document.querySelector(".to").value;
+let currency = document.querySelector(".form-check-input");
 
-let fromDate = document.querySelector("#from");
-let toDate = document.querySelector("#to");
-let currency = document.querySelector(".form-check");
+console.log("this is from currency:", currency)
+console.log("this is from toDate:", toDate)
+console.log("this is from fromDate:", fromDate)
+// let fromDate = undefined;
+// let toDate = undefined;
+// let currency = "USD"
 
-document.querySelector("#boton").addEventListener("click", function () {
-  fromDate = fromDate.textContent;
-  toDate = toDate.value;
-  currency = currency.value;
-  console.log(fromDate);
+
+
+document.querySelector("#button").addEventListener("click", function () {
+  fromDate = document.getElementById("from").valueAsDate;
+  toDate = document.querySelector("#to").innerText;
+  currency = document.querySelector(".form-check-input").textContent;
+  console.log("this is from two:", fromDate);
 });
 
+
+
+
 // fromDate = fromDate.replace("/", "-");
-// toDate = "07-01-2022";
-// currency = "USD";
+// console.log("this is fromDate", fromDate)
+fromDate = ("07-01-2021".slice(-4)) + "-" +("07-01-2021".slice(0, -5))
+// fromDate = "2021-07-01"
+toDate = "2022-07-01";
+currency = "USD";
+
+
+/// from here is working ///
 
 let apiUrlQuery = `http://api.coindesk.com/v1/bpi/historical/close.json?start=${fromDate}&end=${toDate}&currency=${currency}`;
-//let apiUrlQuery = `http://api.coindesk.com/v1/bpi/historical/close.json`
+// let apiUrlQuery = `http://api.coindesk.com/v1/bpi/historical/close.json`
 
-// document.querySelector("#from").onchange = () => {
-//   updateChart();
-// }
 
-// document.querySelector("#to").onchange = () => {
-//   updateChart();
-// }
-// document.querySelector(".form-check").onchange = () => {
-//   updateChart();
-// }
-
-// function updateChart() {
-//   fromDate;
-//   toDate;
-//   currency;
-//   axios
-//     .get(
-//       `http://api.coindesk.com/v1/bpi/historical/close.json?start=${fromDate}&end=${toDate}&currency=${currency}`
-//     )
-//     .then((response) => {
-//       drawCharts(
-//         Object.keys(response.data.bpi),
-//         Object.values(response.data.bpi)
-//       );
-//       document.getElementById("max").innerText = Math.max.apply(
-//         Math,
-//         Object.values(response.data.bpi)
-//       );
-//       document.getElementById("min").innerText = Math.min.apply(
-//         Math,
-//         Object.values(response.data.bpi)
-//       );
-//     });
-// }
-
+/////////////////////////////////////////////////////////////////////////////////////////////7
 const printChart = (stockData) => {
   const dailyData = stockData.bpi;
   // x axis
@@ -80,44 +63,8 @@ const printChart = (stockData) => {
     },
   });
 };
+///////////////////////////////////////////////////////////////////////////////////////////
 
-// const printChart = (stockData) => {
-//   const dailyData = stockData.bpi;
-//   // x axis
-//   const stockDates = Object.keys(dailyData);
-//   console.log(stockDates);
-//   // y axis
-//   const stockPrices = Object.values(dailyData);
-//   // draw the chart
-//   const ctx = document.querySelector("#myChart").getContext("2d");
-
-//   new Chart(ctx, {
-//     type: "line",
-//     data: {
-//       // x axis
-//       labels: stockDates,
-//       datasets: [
-//         {
-//           label: "Bitcoin Price Index",
-//           backGroundColor: "rgba(0, 0, 0",
-//           borderColor: "rgb(60, 180, 171",
-//           // y axis
-//           data: stockPrices,
-//         },
-//       ],
-//     },
-//   });
-// };
-
-// axios
-//   .get(apiUrl)
-//   .then((response) => {
-//     //console.log(response.data.bpi);
-//     printChart(response.data);
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
 
 axios
   .get(apiUrlQuery)
